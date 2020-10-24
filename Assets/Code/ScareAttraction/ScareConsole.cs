d6a2c3cb355083d73a2fc6620e82c5cfc9946a8c
Tyler Devogel <devogelt@gmail.com>
2020-10-24 17:11:25 -0400

    Revert "Merge branch 'Bill'"
    This reverts commit d33bf2f8892e53bdac125e6e2f573bd5bdc8156a, reversing
    changes made to 7b50a834e70199bab5da7dabbfa7e570e3f5dbb8.

--- Assets/Code/ScareAttraction/ScareConsole.cs
+++ Assets/Code/ScareAttraction/ScareConsole.cs
@@ -18,17 +18,14 @@ public class ScareConsole : MonoBehaviour
     [SerializeField] private Timer cooldownTimer;
     public Timer CooldownTimer { get => cooldownTimer; set => cooldownTimer = value; }
 
-    //[SerializeField] private TMP_Text status_Text;
-    //public TMP_Text Status_Text { get => status_Text; set => status_Text = value; }
+    [SerializeField] private TMP_Text status_Text;
+    public TMP_Text Status_Text { get => status_Text; set => status_Text = value; }
 
-    //[SerializeField] private TMP_Text count_Text;
-    //public TMP_Text Count_Text { get => count_Text; set => count_Text = value; }
+    [SerializeField] private TMP_Text count_Text;
+    public TMP_Text Count_Text { get => count_Text; set => count_Text = value; }
 
     [SerializeField] private ScareAttraction scareAttraction;
     public ScareAttraction ScareAttraction { get => scareAttraction; set => scareAttraction = value; }
-
-    [SerializeField] private bool attractionRunning;
-    public bool AttractionRunning { get => attractionRunning; set => attractionRunning = value; }
 
     private void Awake()
     {
@@ -45,28 +42,20 @@ public class ScareConsole : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (cooldownTimer.Finished)
+        count_Text.text = cooldownTimer.GetIntTime().ToString();
+        if(cooldownTimer.Finished == true)
         {
-            scareAttraction.gameObject.SetActive(false);
+            status_Text.text = "READY";
+            count_Text.text = "";
         }
-        else
-        {
-            scareAttraction.gameObject.SetActive(true);
-        }
-        //count_Text.text = cooldownTimer.GetIntTime().ToString();
-        //if(cooldownTimer.Finished == true)
-        //{
-        //    status_Text.text = "READY";
-        //    count_Text.text = "";
-        //}
     }
 
     private void OnFire(InputAction.CallbackContext callbackContext)
     {
         Debug.Log(gameObject.name + " Interaction!");
-
         if (cooldownTimer.Finished)
         {
+            scareAttraction.FireScare();
             cooldownTimer.ResetTimer();
         }
         else
